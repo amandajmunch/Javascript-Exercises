ab6423bb926489256a4c7ae6e8eb420046002cc7
Amanda Munch <amandajmunch@gmail.com>
2017-08-15 12:11:00 -0400

    added reverse string

--- problems.js
+++ problems.js
@@ -36,3 +36,14 @@ function wordCount(str){
 
   console.log(wordCounts);
     };
+
+
+// Reverse a string
+function reverseString(str) {
+  const newString = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+      newString += str[i];
+  }
+    return newString;
+}
+reverseString('hello');
