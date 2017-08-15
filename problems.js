8a7b0f9d288f40599590b2db8a03f91ba2433d8d
Amanda Munch <amandajmunch@gmail.com>
2017-08-15 10:44:12 -0400

    first push

---
+++ problems.js
@@ -0,0 +1,23 @@
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
+
+var sum = 0;
+for(let i = 0; i < 1000; i++){
+  if(i % 3 ===0 || i % 5 === 0){
+    sum += i;
+  }
+}
+console.log(sum);
+
+// JavaScript function that reverse a number.
+function reverseANumber(num){
+  num = num + "";
+  return num.split("").reverse().join("");
+}
+
+// Javascript function to remove duplicates from array
+
+var arr = [1,2,5,2,4,1,2,1];
+
+arr = arr.filter( function( item, index, inputArray ) {
+           return inputArray.indexOf(item) == index;
+    });
