c817b7291169664b248f2c9a9e8b10a66740c234
Amanda Munch <amandajmunch@gmail.com>
2017-11-01 17:27:23 -0400

    longest word function

--- problems.js
+++ problems.js
@@ -47,3 +47,21 @@ function reverseString(str) {
     return newString;
 }
 reverseString('hello');
+
+
+// Find longest word in a string
+
+function longestWord(str){
+  let words = string.split(' ');
+  let longestWord = 0;
+  for (var i = 0; i < words.length; i++){
+    if (words[i].length > longestWord){
+      longestWord = words[i].length;
+    }
+  }
+  return longestWord;
+}
+
+
+
+
